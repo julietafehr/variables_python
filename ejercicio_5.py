7426ccfe5d20f04b6c575b2bb139274a55032a3c
julietafehr <90472706+julietafehr@users.noreply.github.com>
2021-09-20 14:41:01 -0300

    Add files via upload

---
+++ ejercicio_5.py
@@ -0,0 +1,34 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese dos palabras y arme combinaciones con ella
+print('Ingrese palabra 1:')
+palabra_1 = str(input())
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input())
+
+palabra_1 = "dias"
+palabra_2 = "soleados"
+
+palabra_1[0:3]
+print(palabra_1[0:3])
+
+palabra_2[0:2]
+print(palabra_2[0:2])
+
+nueva_palabra = "diaso"
+print(nueva_palabra)
+
+# De la primera palabra tome las primeras tres letras, utilice el operador :
+# De la segunda palabra tome las primeras dos letras, utilice el operador :
+# Formar una nueva palabra con los recortes solicitados
+# Imprima en pantalla los resultados
