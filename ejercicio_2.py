4e6391c2cbae6914b77383a09aeaeee614e8644c
julietafehr <90472706+julietafehr@users.noreply.github.com>
2021-09-20 14:29:22 -0300

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,53 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+
+# Alumno: Imprima en pantalla los dos números decimales solicitados
+# print(....)
+
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
+
+print("Ingrese por consola el primer número decimal a operar:")
+numero_1 = float(input())
+print("Ingrese por consola el segundo número decimal a operar:")
+numero_2 = float(input())
+print(numero_1, numero_2)
+
+# Suma
+numero_1 = 3.5
+numero_2 = 1.2
+suma = numero_1 + numero_2
+print("El resultado de sumar 3.5 y 1.2 es: ", suma)
+
+# Resta
+numero_1 = 3.5
+numero_2 = 1.2
+resta = numero_1 - numero_2
+print("El resultado de restar 3.5 menos 1.2 es: ", resta)
+
+# División
+numero_1 = 3.5
+numero_2 = 1.2
+división = numero_1 / numero_2
+print("El resultado de dividir 3.5 por 1.2 es: ", división)
+
+# Multiplicación
+numero_1 = 3.5
+numero_2 = 1.2
+multiplicacion = numero_1 * numero_2
+print("El resultado de multiplicar 3.5 por 1.2 es: ", multiplicacion)
+
---
+++ ejercicio_4.py
@@ -0,0 +1,35 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+# Si desea puede modificar el código para ingresar más palabras
+print('Ingrese palabra 1:')
+palabra_1 = str(input())
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input())
+
+print('Ingrese palabra 3:')
+palabra_3 = str(input())
+
+print("Ingrese palabra 4: ")
+palabra_4 = str(input())
+
+texto = "Objeto Volador No Identificado"
+print(texto)
+
+acronimo = texto[0] + texto[7] + texto[15] + texto[18]
+print(acronimo)
+
+
+# De cada palabra debe tomar la primera letra y armar el acrónimo
+# Ejemplo: Alumbrado, barrido y limpieza --> ABL
+# Imprimir el resultado en pantalla
